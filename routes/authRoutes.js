a1abf957154c51127162ffab6e6e0f7e999f8711
Goutam Chandra Gharami <dev.goutam05@gmail.com>
2025-08-24 19:59:17 +0600

    Create authRoutes.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+// Author: Goutam Chandra Gharami
+// Email: dev.goutam05@gmail.com
+
+import express from "express";
+import { register, login, logout } from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+
+export default router;
